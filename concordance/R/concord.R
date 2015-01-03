7a5261cf89a345561ed154af53598a3e90f6c4ca
zucxjo0415 <zucxjo@gmail.com>
2015-01-03 18:41:35 -0500

    Added HS + truncated code concordance
    - Added all HS versions (1988/92, 1996, 2002, 2007, 2012)
    - Algorithm will now auto-detect and perform concordance for truncated
    codes
    - More examples added to documentation
    - References added to documentation

--- concordance/R/concord.R
+++ concordance/R/concord.R
@@ -3,6 +3,7 @@ function (sourcevar, origin, destination, warn=FALSE){
     # Sanity check
     origin_codes <- names(concord_data)
     destination_codes <- names(concord_data)
+    # Allow origin / destination to be entered in any case
     origin <- toupper(origin)
     destination <- toupper(destination)
     if (!origin %in% origin_codes){stop("Origin code not supported")}
@@ -10,12 +11,26 @@ function (sourcevar, origin, destination, warn=FALSE){
     
     dict <- na.omit(concord_data[,c(origin, destination)])
     
-    # Prepare output vector
-    dest_var <- rep(NA, length(sourcevar))
-    # All but regex-based operations
-    matches <- which(dict[,origin] %in% sourcevar)
-    differs <- sapply(dict[matches,origin], function(x) which(sourcevar %in% x))
+    # Remove duplicated inputs 
+    sourcevar <- sourcevar[!duplicated(sourcevar)]
+    # If input is shorter than expected, pad it.
+    isShort <- sapply(sourcevar, function(x) nchar(x) < lengths[origin])
+    shorts <- sourcevar[isShort]
+    fulls <- sourcevar[!isShort]
+    l <- lengths[origin]
+    pads <- sapply(shorts, function(x) (as.integer(x) * 10^(l-nchar(x))):((as.integer(x)+1) * 10^(l-nchar(x)) - 1))
+    sourcevar <- c(fulls, unlist(pads))
+    # Now deal with leading zeroes
+    sourcevar <- as.integer(sourcevar[!duplicated(sourcevar)])
     
-    dest_var <- lapply(1:length(sourcevar), function(x) dict[matches[which(differs %in% x)],destination])
+    # Vector operations currently disabled.
+    # dest_var <- rep(NA, length(sourcevar)
+    matches <- which(dict[,origin] %in% sourcevar)
+    # differs <- sapply(dict[matches,origin], function(x) which(sourcevar %in% x))
+    # dest_var <- lapply(1:length(sourcevar), function(x) dict[matches[which(differs %in% x)],destination])
+    
+    dest_var <- dict[matches, destination]
+    dest_var <- dest_var[!duplicated(dest_var)]
+    
     return(dest_var)
 }
