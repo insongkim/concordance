6f5ba73faaa7e68cccc8ca7a56c7ac5c9ec622de
zucxjo0415 <zucxjo@gmail.com>
2015-06-17 22:05:46 -0700

    Changes to sigma
    Added lookup via HTS = HS for the US (new data added to sigmatab table
    and new code added to sigma() function.)

--- concordance/R/sigma.R
+++ concordance/R/sigma.R
@@ -1,27 +1,38 @@
 utils::globalVariables("sigmatab")
 sigma <-
-  function (sourcevar, origin, country="USA") {
+  function (sourcevar, origin, country="USA", use_SITC=FALSE, give_avg=TRUE) {
     # Sanity checks (more folded into call to concord)
     country <- toupper(country)
     if (!country %in% unique(sigmatab$iso3)) {stop("No data for specified country")}
-
-    # Concord to SITC3 for USA, HS3 for others
     viacode <- "HS3"
-    if (country == "USA") { viacode <- "SITC3"}
+    if (country == "USA" && use_SITC) { viacode <- "SITC3" }
     if (!viacode == origin) {sourcevar <- concord(sourcevar, origin, viacode)}
-    # UGH THIS HACK
+    
     if (viacode == "HS3") { 
-      sourcevar <- concord(sourcevar, origin, viacode)
-      sourcevar <- sprintf("%06d", as.integer(sourcevar))
-      sourcevar <- sapply(sourcevar, function(x) substr(x,1,3))}
+      # as with many other things, the US just does its own thing
+      if (country == "USA") { 
+        sourcevar <- concord(sourcevar, origin, viacode)
+        sourcevar <- sprintf("%06d", as.integer(sourcevar))
+        l <- 10
+        sourcevar <- sapply(sourcevar, function(x) (suppressWarnings(as.integer(x)) * 10^(l-nchar(x))):((suppressWarnings(as.integer(x))+1) * 10^(l-nchar(x)) - 1))
+        sourcevar <- suppressWarnings(unlist(sourcevar))
+      } else {
+        # UGH THIS HACK
+          sourcevar <- concord(sourcevar, origin, viacode)
+          sourcevar <- sprintf("%06d", as.integer(sourcevar))
+          sourcevar <- sapply(sourcevar, function(x) substr(x,1,3))
+      }
+    }
     
     # Now deal with leading zeroes (and remove duplicated inputs)
-    via <- as.integer(sourcevar[!duplicated(sourcevar)])
+    via <- as.numeric(sourcevar[!duplicated(sourcevar)])
     
     contmat <- (sigmatab$iso3 == country)
-    matches <- (suppressWarnings(as.integer(sigmatab[,viacode])) 
+    matches <- (suppressWarnings(as.numeric(sigmatab[,viacode])) 
                      %in% via)
     
-    ctv <- sigmatab[which(contmat & matches),"sigma"]
+    ctv <- suppressWarnings(sigmatab[which(contmat & matches),"sigma"])
+    na.omit(ctv) -> ctv
+    if (give_avg) { mean(ctv) -> ctv }
     return(ctv)
   }
