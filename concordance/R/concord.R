8b3b31b12435607172ffb14e0e9f1ae7add56f80
zucxjo0415 <zucxjo@gmail.com>
2015-06-22 23:40:57 -0700

    Added concordance to BEC, SIC, NAICS
    Descriptions for these codes via the desc function is not available yet,
    but I'll work on it (it'll probably be the last feature I add to the
    package.)

--- concordance/R/concord.R
+++ concordance/R/concord.R
@@ -7,39 +7,47 @@ function (sourcevar, origin, destination){
     # Allow origin / destination to be entered in any case
     origin <- toupper(origin)
     destination <- toupper(destination)
-    if (!origin %in% origin_codes){stop("Origin code not supported")}
-    if (!destination %in% destination_codes){stop("Destination code not supported")}
     
-    dict <- na.omit(concord_data[,c(origin, destination)])
-    
-    # Remove duplicated inputs 
-    # sourcevar <- sourcevar[!duplicated(sourcevar)]
-    # If input is shorter than expected, pad it.
-    isShort <- sapply(sourcevar, function(x) nchar(x) < lengths[origin])
-    shorts <- sourcevar[isShort]
-    fulls <- sourcevar[!isShort]
-    l <- lengths[origin]
-    pads <- sapply(shorts, function(x) (as.integer(x) * 10^(l-nchar(x))):((as.integer(x)+1) * 10^(l-nchar(x)) - 1))
-    sourcevar <- c(fulls, unlist(pads))
-    # If input is longer than expected, truncate it.
-    isLong <- sapply(sourcevar, function(x) nchar(x) > lengths[origin])
-    longs <- sourcevar[isLong]
-    okays <- sourcevar[!isLong]
-    l <- lengths[origin]
-    truncs <- sapply(longs, function(x) floor(as.integer(x) / 10^(nchar(x)-l)) )
-    sourcevar <- c(okays, unlist(truncs))
-    # Now deal with leading zeroes (and remove duplicated inputs)
-    sourcevar <- as.integer(sourcevar[!duplicated(sourcevar)])
-    
-    
-    # Vector operations currently disabled.
-    # dest_var <- rep(NA, length(sourcevar)
-    matches <- which(dict[,origin] %in% sourcevar)
-    # differs <- sapply(dict[matches,origin], function(x) which(sourcevar %in% x))
-    # dest_var <- lapply(1:length(sourcevar), function(x) dict[matches[which(differs %in% x)],destination])
-    
-    dest_var <- dict[matches, destination]
-    dest_var <- dest_var[!duplicated(dest_var)]
-    
-    return(dest_var)
+    # NAICS / SIC concordance (via HS) are in a separate table
+    # to avoid ballooning existing tables (since these have many many-to-many's)
+    if (origin %in% long_codes || destination %in% long_codes) { 
+      return(extend_concord(sourcevar, origin, destination)) }
+    else {
+      if (!origin %in% origin_codes){stop("Origin code not supported")}
+      if (!destination %in% destination_codes){stop("Destination code not supported")}
+      
+      dict <- na.omit(concord_data[,c(origin, destination)])
+      
+      # Remove duplicated inputs 
+      # sourcevar <- sourcevar[!duplicated(sourcevar)]
+      # If input is shorter than expected, pad it.
+      if (origin != 'BEC') {
+        isShort <- sapply(sourcevar, function(x) nchar(x) < lengths[origin])
+        shorts <- sourcevar[isShort]
+        fulls <- sourcevar[!isShort]
+        l <- lengths[origin]
+        pads <- sapply(shorts, function(x) (as.integer(x) * 10^(l-nchar(x))):((as.integer(x)+1) * 10^(l-nchar(x)) - 1))
+        sourcevar <- c(fulls, unlist(pads))
+      }
+      # If input is longer than expected, truncate it.
+      isLong <- sapply(sourcevar, function(x) nchar(x) > lengths[origin])
+      longs <- sourcevar[isLong]
+      okays <- sourcevar[!isLong]
+      l <- lengths[origin]
+      truncs <- sapply(longs, function(x) floor(as.integer(x) / 10^(nchar(x)-l)) )
+      sourcevar <- c(okays, unlist(truncs))
+      # Now deal with leading zeroes (and remove duplicated inputs)
+      sourcevar <- sourcevar[!duplicated(sourcevar)]
+      
+      # Vector operations currently disabled.
+      # dest_var <- rep(NA, length(sourcevar)
+      matches <- which(dict[,origin] %in% sourcevar)
+      # differs <- sapply(dict[matches,origin], function(x) which(sourcevar %in% x))
+      # dest_var <- lapply(1:length(sourcevar), function(x) dict[matches[which(differs %in% x)],destination])
+      
+      dest_var <- dict[matches, destination]
+      dest_var <- dest_var[!duplicated(dest_var)]
+      
+      return(dest_var)
+    }
 }
---
+++ concordance/R/extend_concord.R
@@ -0,0 +1,46 @@
+utils::globalVariables(c("concord_data","concord_long","lengths","long_codes"))
+extend_concord <-
+  function (sourcevar, origin, destination){
+    # Sanity check
+    origin_codes <- c(names(concord_data), long_codes)
+    destination_codes <- c(names(concord_data), long_codes)
+    if (!origin %in% origin_codes){stop("Origin code not supported")}
+    if (!destination %in% destination_codes){stop("Destination code not supported")}
+    
+    viacodes <- sourcevar
+    via <- destination
+    if (!origin %in% long_codes && origin != 'HS') { 
+      viacodes <- concord(sourcevar, origin, 'HS') 
+      origin <- 'HS' }
+    if (!destination %in% long_codes) { via <- 'HS' }
+    dict <- na.omit(concord_long[,c(origin, via)])
+    
+    # Remove duplicated inputs 
+    # sourcevar <- sourcevar[!duplicated(sourcevar)]
+    l <- lengths[origin]
+    if (origin == 'HS') { l <- 10 }
+    # If input is shorter than expected, pad it.
+    if (origin != 'BEC') {
+      isShort <- sapply(viacodes, function(x) nchar(x) < l)
+      shorts <- viacodes[isShort]
+      fulls <- viacodes[!isShort]
+      pads <- sapply(shorts, function(x) (as.integer(x) * 10^(l-nchar(x))):((as.integer(x)+1) * 10^(l-nchar(x)) - 1))
+      viacodes <- c(fulls, unlist(pads))
+    }
+    # If input is longer than expected, truncate it.
+    isLong <- sapply(viacodes, function(x) nchar(x) > lengths[origin])
+    longs <- viacodes[isLong]
+    okays <- viacodes[!isLong]
+    truncs <- sapply(longs, function(x) floor(as.integer(x) / 10^(nchar(x)-l)) )
+    viacodes <- c(okays, unlist(truncs))
+    # Now deal with leading zeroes (and remove duplicated inputs)
+    viacodes <- viacodes[!duplicated(viacodes)]
+    
+    matches <- which(dict[,origin] %in% viacodes)
+    
+    dest_var <- dict[matches, via]
+    dest_var <- dest_var[!duplicated(dest_var)]
+    if (via != destination) { dest_var <- concord(dest_var, via, destination) }
+    
+    return(dest_var)
+  }
