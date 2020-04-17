ed8885811b9733c9f440315e23dd43d8e04107cc
Steven Liao <steven.liao@ucr.edu>
2020-04-17 15:23:43 -0700

    Restructure package folder

--- listSigma.R
+++
@@ -1,5 +0,0 @@
-utils::globalVariables(c("BWsigma", "sigma_data"))
-
-listSigma <- function(sourcevar, origin, country="USA", use_SITC=FALSE) {
-  return(lapply(sourcevar, sigma, origin=origin, country=country, use_SITC=use_SITC, give_avg=FALSE))
-}
