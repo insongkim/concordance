06216c0ab856980bdea557a77c2ec460eb62299a
haocharliezhang <haocharliezhang@gmail.com>
2020-07-26 17:04:14 -0400

    file adjustment
    adjust the bea concordance file

--- data-raw/clean-detailed-upstream.R
+++ data-raw/clean-detailed-upstream.R
@@ -54,7 +54,32 @@ bea2002_naics2002$NAICS2002_2d <- substr(bea2002_naics2002$NAICS2002_4d , start 
 
 # save
 save(bea2002_naics2002 ,
-     file = "./data/bea2002_naics2002 .RData", compress = "xz")
+     file = "./data/bea2002_naics2002.RData", compress = "xz")
+
+################################################################################
+# BEA2007 to NAICS2007
+################################################################################
+# load concordance data:
+# https://apps.bea.gov/iTable/index_industry_io.cfm, follow "Make-Use Framework Tables"-"Use Table"-"After Redefinitions (Producer's Prices)"-"Detailed level (2007, 2012)"
+# note that bea2007 concords with naics 2012, so has to merge with naics2007
+bea2007_naics2007 <- read_csv("./data-raw/2012_BEA_to_2012_NAICS.csv", col_types = cols(.default = "c"))
+
+# check names
+names(bea2007_naics2007)
+
+# adjust bea2012 to bea2007
+bea2007_naics2007$BEA2007 <- bea2007_naics2007$BEA2012
+bea2007_naics2007$NAICS2007_6d <- concord(bea2007_naics2007$NAICS2012_6d, "NAICS2012", "NAICS2007", dest.digit = 6, all = FALSE)
+
+bea2007_naics2007$NAICS2007_4d <- substr(bea2007_naics2007$NAICS2007_6d , start = 1, stop = 4)
+bea2007_naics2007$NAICS2007_2d <- substr(bea2007_naics2007$NAICS2007_4d , start = 1, stop = 2)
+
+# get rid of the 2012 variables
+bea2007_naics2007 <- bea2007_naics2007[, -c(1,2)]
+
+# save
+save(bea2007_naics2007 ,
+     file = "./data/bea2007_naics2007.RData", compress = "xz")
 
 ################################################################################
 # BEA2012 to NAICS2012
@@ -71,7 +96,7 @@ bea2012_naics2012$NAICS2012_2d <- substr(bea2012_naics2012$NAICS2012_4d , start 
 
 # save
 save(bea2012_naics2012 ,
-     file = "./data/bea2012_naics2012 .RData", compress = "xz")
+     file = "./data/bea2012_naics2012.RData", compress = "xz")
 
 ################################################################################
 # Antras and Chor (2012, 2013) 
@@ -99,24 +124,4 @@ colnames(upstream_us_detailed)[c(1,2)] <- c("CODE", "GVC_Ui")
 # save
 save(upstream_us_detailed,
      file = "./data/upstream_us_detailed.RData", compress = "xz")
-
-################################################################################
-# BEA Concordance between 2002 and 2012  
-################################################################################
-# load dataset
-bea2002_naics2002 <- read_csv("./data-raw/2002_BEA_to_2002_NAICS.csv", col_types = cols(.default = "c"))
-bea2012_naics2012 <- read_csv("./data-raw/2012_BEA_to_2012_NAICS.csv", col_types = cols(.default = "c"))
-
-# match naics 2002 with 2012
-bea2002_naics2002 <- subset(bea2002_naics2002, NAICS2002_6d != "n.a.")
-bea2002_naics2002$NAICS2012_6d <- concord(bea2002_naics2002$NAICS2002_6d, "NAICS2002", "NAICS2012", dest.digit = 6, all = FALSE)
-
-# create bea concordance
-bea2002_bea2012 <- full_join(bea2002_naics2002, bea2012_naics2012, by = "NAICS2012_6d")
-bea2002_bea2012 <- bea2002_bea2012[,c(1,4)]
-bea2002_bea2012 <- bea2002_bea2012 %>% distinct()
-bea2002_bea2012 <- bea2002_bea2012[complete.cases(bea2002_bea2012),]
-
-save(bea2002_bea2012,
-     file = "./data/bea2002_bea2012.RData", compress = "xz")
 
